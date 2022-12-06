bad6e4d80a6d979c33a81a487beebb60438fd9ae
karim <450906k@gmail.com>
2022-12-06 21:43:37 +0500

    async script

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -8,6 +8,6 @@ export default function Layout({children} : ScriptProps) {
             <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossOrigin="anonymous" />
         </Head>
         {children}
-        <script src="https://cdn.jsdelivr.net/npm/react-bootstrap@next/dist/react-bootstrap.min.js" crossOrigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/react-bootstrap@next/dist/react-bootstrap.min.js" crossOrigin="anonymous" async></script>
     </>
 }

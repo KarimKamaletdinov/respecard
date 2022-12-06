a5f10a147c6e609f157745a30dde0df0f934c7f1
karim <450906k@gmail.com>
2022-12-06 20:45:13 +0500

    react-bootstrap & simple content view

---
+++ components/Layout.tsx
@@ -0,0 +1,13 @@
+import Head from "next/head";
+import { ScriptProps } from "next/script";
+
+export default function Layout({children} : ScriptProps) {
+    return <>
+        <Head>
+            <title>Сравнение карт Tinkoff</title>
+            <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossOrigin="anonymous" />
+        </Head>
+        {children}
+        <script src="https://cdn.jsdelivr.net/npm/react-bootstrap@next/dist/react-bootstrap.min.js" crossOrigin="anonymous"></script>
+    </>
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,34 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import { Accordion, Container } from 'react-bootstrap'
+import Layout from '../components/Layout'
+import styles from '../styles/app.module.css'
 
 export default function Home() {
-  return (<>respecard</>
-  )
+  return <Layout>
+    <Container className='p-2'>
+      <h1>Tinkoff Black</h1>
+      <Accordion>
+        <div className={`d-inline-block ${styles.pictureContainer}`}>
+          <img src="./tinkoff_black.jpg" className={`v-100 ${styles.picture}`} />
+        </div>
+        <Accordion.Item eventKey="0">
+          <Accordion.Header>
+            Как заказать карту
+          </Accordion.Header>
+          <Accordion.Body>
+            <ol>
+              <li>Перейдите по ссылке ниже.</li>
+              <li>Заполните анкету.</li>
+              <li>Вам позвонит сотрудник банка.</li>
+              <li>Сотрудник банка спросит адрес, и в какое время вы бы хотели встретиться с представителем
+                Tinkoff.
+                (Представитель Tinkoff – человек, который доставит вам карту)</li>
+              <li>В назначеное вами время представитель Tinkoff будет на назначеном вами адресе.</li>
+              <li>Вы должны прийти на встречу <b>с паспортом</b>.</li>
+              <li>Вам отдадут карту, и вы подпишете договор с банком.</li>
+            </ol>
+          </Accordion.Body>
+        </Accordion.Item>
+      </Accordion>
+    </Container>
+  </Layout>
 }

80231ab23bb0597b8f6938e7002d55cf591e6bfa
karim <450906k@gmail.com>
2023-01-06 20:44:32 +0500

    tooltips

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,7 @@
 import type { AppProps } from 'next/app'
+import '../styles/theme.css'
+import '../styles/grid.css'
+import '../styles/grid-mobile.css'
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
